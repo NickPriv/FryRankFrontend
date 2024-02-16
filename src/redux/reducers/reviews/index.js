efc335e3d1476c24aaca5ca182480d8b87529023
Nicholas Privitera <nicholaspriv@gmail.com>
2024-02-15 21:41:29 -0800

    Resolving three bugs during the review creation process: 1) Score can only be in 1 to 10 range, 2) Current review state is cleared after submission, and 3) Empty strings are converted into null values during review value updates

--- src/redux/reducers/reviews/index.js
+++ src/redux/reducers/reviews/index.js
@@ -14,7 +14,7 @@ export const initialState = {
   currentReview: {
     "restaurantId": null,
     "authorId": null,
-    "score": null,
+    "score": "10", // Initialize score to 10 because select input defaults value to 10
     "title": null,
     "body": null
   },
@@ -48,6 +48,11 @@ export default (state = initialState, action) => {
         case types.CREATE_REVIEW_FOR_RESTAURANT_REQUEST: {
             return {
                 ...state,
+                currentReview: {
+                    // Reset the current review, except for the restaurantId, which gets set on page load
+                    ...initialState.currentReview,
+                    restaurantId: state.currentReview.restaurantId
+                }
             };
         }
 
@@ -72,7 +77,7 @@ export default (state = initialState, action) => {
                 ...state,
                 currentReview: {
                     ...state.currentReview,
-                    [action.name]: action.value
+                    [action.name]: action.value != "" ? action.value : null
                 }
             }
         }
