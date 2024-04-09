9861440666a40a9701dd9382489ed85ae37ec8b1
Nicholas Privitera <nicholaspriv@gmail.com>
2024-04-08 20:50:22 -0700

    Use location for text search

--- src/containers/Restaurants/index.js
+++ src/containers/Restaurants/index.js
@@ -8,20 +8,22 @@ const mapStateToProps = (state) => {
     return {
         restaurants: state.restaurantsReducer.restaurants,
         error: state.restaurantsReducer.error,
-        currentSearchQuery: state.restaurantsReducer.searchQuery
+        currentSearchQuery: state.restaurantsReducer.searchQuery,
+        location: state.restaurantsReducer.location
     }
 }
 
 const mapDispatchToProps = {
     getRestaurants: restaurantsActions.startGetRestaurantsRequest,
-    updateSearchQuery: restaurantsActions.updateSearchQuery
+    updateSearchQuery: restaurantsActions.updateSearchQuery,
+    setLocation: restaurantsActions.setLocation,
 };
 
 export default compose(
     connect(mapStateToProps, mapDispatchToProps),
     lifecycle({
         componentDidMount() {
-            const { getRestaurants } = this.props;
+            const { getRestaurants, setLocation } = this.props;
 
             if (navigator.geolocation) {
                 navigator.permissions
@@ -31,6 +33,7 @@ export default compose(
                             navigator.geolocation.getCurrentPosition (
                                 (position) => {
                                     getRestaurants(FRENCH_FRIES_TEXT_QUERY, position.coords);
+                                    setLocation(position.coords);
                                 },
                                 (error) => {
                                     getRestaurants(FRENCH_FRIES_TEXT_QUERY);
