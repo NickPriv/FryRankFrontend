5c032162d03fbe78baedb67dc55ece8bfebc6c72
oxyflush <dan.zhan10@gmail.com>
2024-05-22 22:49:24 -0700

    Remove duplicate constants (#37)

--- src/constants.js
+++ src/constants.js
@@ -16,13 +16,3 @@ export const WELCOME_MESSAGE = "Bonjour, my name is Monsieur LeFry. Bienvenue to
     "meticulously sampling each fry, and dissecting their every nuance? This is my world, the world of a French Fry food critic. " +
     "Join me as we explore the art of evaluating, appreciating, and savoring the remarkable world of French Fries. " +
     "Ok, enough talk. Off you go now!"
-
-export const FRENCH_FRIES_TEXT_QUERY = "french fries";
-
-export const WELCOME_MESSAGE = "Bonjour, my name is Monsieur LeFry. Bienvenue to my website, FryRank. " +
-    "Crisp, golden, and irresistible, French Fries have held a special place in my heart ever since I was a wee garçon. " +
-    "Have you ever wondered what it's like to immerse yourself in the world of French Fries, traversing countless eateries, " +
-    "meticulously sampling each fry, and dissecting their every nuance? This is my world, the world of a French Fry food critic. " +
-    "Join me as we explore the art of evaluating, appreciating, and savoring the remarkable world of French Fries. " +
-    "Ok, enough talk. Off you go now!"
-
