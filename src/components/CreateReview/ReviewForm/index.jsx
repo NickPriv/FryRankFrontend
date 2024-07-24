c63b0185087189e48c25f276c625e6cb3c9e95cc
oxyflush <dan.zhan10@gmail.com>
2024-07-23 21:55:45 -0700

    Prevent create reviews page from being edited while logged out (#49)
    This commit adds logic to render an AI art image and message that prompts
    the user to log in if not logged in already. Previously, users were able
    to bypass the previous restaurants page and directly access the create
    reviews page if they had access to the URL or refreshed the create reviews
    page (which logs them out because we don't save cookies) and are able to
    edit the text boxes to create a review without being logged in. This leads
    to a bug where if they click log in, the page refreshes, their text is
    still present, but no updates to the create reviews page have been
    recorded. This causes the review to fail to be submitted even though the
    tet boxes appear to be populated.

--- src/components/CreateReview/ReviewForm/index.jsx
+++ src/components/CreateReview/ReviewForm/index.jsx
@@ -2,6 +2,8 @@ import { PropTypes } from 'prop-types';
 import { Form, FormGroup, Input, Label } from 'reactstrap';
 
 import { Button, LinkButton } from '../../Common';
+import FryposalLoginImage from "../../../Fryposal.png";
+import './style.css';
 
 const propTypes = {
     currentRestaurant: PropTypes.object.isRequired,
@@ -16,6 +18,7 @@ const propTypes = {
 const ReviewForm = ({ createReview, currentRestaurant, currentReview, updateCurrentReview, loggedIn, givenName, accountId }) => {
 
     return (
+        <div>
         <Form
             onChange={(event) => {
                 if (!currentReview.authorId && givenName) {
@@ -27,69 +30,71 @@ const ReviewForm = ({ createReview, currentRestaurant, currentReview, updateCurr
                 updateCurrentReview(event.target.name, event.target.value);
             }}
         >
-            <FormGroup>
-                <Label for="nameInput">
-                    Name
-                </Label>
-                <Input
-                    id="nameInput"
-                    name="authorId"
-                    value={givenName}
-                    type="textarea"
-                    disabled="true"
-                />
-            </FormGroup>
-            <FormGroup>
-                <Label for="scoreInput">
-                    Score
-                </Label>
-                <Input type="select" name="score" id="scoreInput" className="form-select">
-                    <option></option>
-                    <option>10</option>
-                    <option>9</option>
-                    <option>8</option>
-                    <option>7</option>
-                    <option>6</option>
-                    <option>5</option>
-                    <option>4</option>
-                    <option>3</option>
-                    <option>2</option>
-                    <option>1</option>
-                </Input>
-            </FormGroup>
-            <FormGroup>
-                <Label for="titleInput">
-                    Title
-                </Label>
-                <Input
-                    id="titleInput"
-                    name="title"
-                    placeholder="A title for your review"
-                    type="textarea"
-                />
-            </FormGroup>
-            <FormGroup>
-                <Label for="bodyInput">
-                    Body
-                </Label>
-                <Input
-                    id="bodyInput"
-                    name="body"
-                    placeholder="Your review text here"
-                    type="textarea"
-                />
-            </FormGroup>
-            { loggedIn ? <Button
+            {loggedIn &&
+                <div><FormGroup>
+                    <Label for="nameInput">
+                        Name
+                    </Label>
+                    <Input
+                        id="nameInput"
+                        name="authorId"
+                        value={givenName}
+                        type="textarea"
+                        disabled="true"
+                    />
+                </FormGroup>
+                    <FormGroup>
+                        <Label for="scoreInput">
+                            Score
+                        </Label>
+                        <Input type="select" name="score" id="scoreInput" className="form-select">
+                            <option></option>
+                            <option>10</option>
+                            <option>9</option>
+                            <option>8</option>
+                            <option>7</option>
+                            <option>6</option>
+                            <option>5</option>
+                            <option>4</option>
+                            <option>3</option>
+                            <option>2</option>
+                            <option>1</option>
+                        </Input>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="titleInput">
+                            Title
+                        </Label>
+                        <Input
+                            id="titleInput"
+                            name="title"
+                            placeholder="A title for your review"
+                            type="textarea"
+                        />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="bodyInput">
+                            Body
+                        </Label>
+                        <Input
+                            id="bodyInput"
+                            name="body"
+                            placeholder="Your review text here"
+                            type="textarea"
+                        />
+                    </FormGroup></div>
+            }
+            {loggedIn ? <Button
                 children='Submit'
                 color='danger'
                 onClick={(event) => {
                     createReview(currentReview)
                 }}
             /> : <Button
-                    children='Log in to Google to submit a review'
-                    color='danger'
-                    disabled='true'
-                />
+                children='Log in to Google to submit a review'
+                color='danger'
+                disabled='true'
+            />
             }
             <LinkButton
                 link={'/restaurants/' + currentRestaurant.id}
@@ -97,7 +102,15 @@ const ReviewForm = ({ createReview, currentRestaurant, currentReview, updateCurr
                 color='secondary'
             />
         </Form>
-    )
+        {!loggedIn &&
+            <div>
+                <img src={FryposalLoginImage} className="fryposal-login-image" alt="fyposal-login-image"/>
+                <br></br>
+                <h4 className="login-requirement-message">"Will you log in for me?"</h4>
+            </div>
+        }
+        </div>
+)
 }
 
 ReviewForm.propTypes = propTypes;
