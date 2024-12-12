e1f19eb3b1576b13fed1db2543d8e45f83416eb3
Alma <aniu@scu.edu>
2024-12-11 21:50:04 -0800

    fix spacing

--- src/components/EditReview/EditReviewModal.jsx
+++ src/components/EditReview/EditReviewModal.jsx
@@ -6,17 +6,17 @@ export default function EditReviewModal({modal, toggle, review, restaurant, onSa
     return (
         <>
             <Modal isOpen={modal} toggle={toggle} >        
-                <ModalHeader toggle={toggle}>Let's edit your review </ModalHeader>
+                <ModalHeader toggle={toggle}> Let's edit your review </ModalHeader>
                 <ModalBody>
-                    <h3>{review.title }</h3> 
-                    <h3>{review.score }</h3> 
-                    <h3>{review.author }</h3>
+                    <h3>{review.title}</h3> 
+                    <h3>{review.score}</h3> 
+                    <h3>{review.author}</h3>
                     <h3>{restaurant.displayName.text}</h3> 
                 </ModalBody>
                 <ModalFooter>
                     <Button color="primary" onClick={toggle}>
                         Save Edit
-                    </Button> { '  '}
+                    </Button> {'  '}
                     <Button color="secondary" onClick={toggle}>
                         Cancel Edit
                     </Button>
