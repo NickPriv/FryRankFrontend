3e558242573a55d860e5b0efb8424eacc433a2fc
Nicholas <nicholaspriv@gmail.com>
2025-03-02 21:31:22 -0800

    Fix score dropdown so that selected score appears in the Score field

--- src/components/Common/ScoreDropdown/index.jsx
+++ src/components/Common/ScoreDropdown/index.jsx
@@ -1,32 +1,27 @@
-import React, { memo } from 'react';
+import React from 'react';
 import { FormGroup, Label, Input } from 'reactstrap';
 
-const ScoreDropdown = memo(({name, id, labelName, value='', onChange = () => {}}) => {
+const ScoreDropdown = () => {
     return (
-        <FormGroup> 
-            <Label for="scoreInput">{labelName}</Label> 
-            <Input 
-                type="select" 
-                name={name}
-                id={id}
-                value = {value}
-                onChange={onChange} 
-                className="form-select" 
-            > 
-                <option value="">Select a {name}</option> 
-                <option value="10">10</option> 
-                <option value="9">9</option> 
-                <option value="8">8</option> 
-                <option value="7">7</option> 
-                <option value="6">6</option> 
-                <option value="5">5</option> 
-                <option value="4">4</option> 
-                <option value="3">3</option> 
-                <option value="2">2</option> 
-                <option value="1">1</option> 
-            </Input> 
+        <FormGroup>
+            <Label for="scoreInput">
+                Score
+            </Label>
+            <Input type="select" name="score" id="scoreInput" className="form-select">
+                <option></option>
+                <option>10</option>
+                <option>9</option>
+                <option>8</option>
+                <option>7</option>
+                <option>6</option>
+                <option>5</option>
+                <option>4</option>
+                <option>3</option>
+                <option>2</option>
+                <option>1</option>
+            </Input>
         </FormGroup>
     );
-});
+};
 
 export default ScoreDropdown;
