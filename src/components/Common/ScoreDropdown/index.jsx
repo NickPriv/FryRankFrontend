ba2d7a4c174303de6c6aa5436770c3412776f5f8
Nicholas Privitera <nicholaspriv@gmail.com>
2025-03-05 20:17:16 -0800

    Using defaultValue instead of value so that score can change when selected on a new review (#136)
    * Fix score dropdown so that selected score appears in the Score field
    
    * Revert "Fix score dropdown so that selected score appears in the Score field"
    
    This reverts commit 3e558242573a55d860e5b0efb8424eacc433a2fc.
    
    * Using defaultValue instead of value so that score can change when selected on a new review

--- src/components/Common/ScoreDropdown/index.jsx
+++ src/components/Common/ScoreDropdown/index.jsx
@@ -1,27 +1,32 @@
-import React from 'react';
+import React, { memo } from 'react';
 import { FormGroup, Label, Input } from 'reactstrap';
 
-const ScoreDropdown = () => {
+const ScoreDropdown = memo(({name, id, labelName, value='', onChange = () => {}}) => {
     return (
-        <FormGroup>
-            <Label for="scoreInput">
-                Score
-            </Label>
-            <Input type="select" name="score" id="scoreInput" className="form-select">
-                <option></option>
-                <option>10</option>
-                <option>9</option>
-                <option>8</option>
-                <option>7</option>
-                <option>6</option>
-                <option>5</option>
-                <option>4</option>
-                <option>3</option>
-                <option>2</option>
-                <option>1</option>
-            </Input>
+        <FormGroup> 
+            <Label for="scoreInput">{labelName}</Label> 
+            <Input 
+                type="select" 
+                name={name}
+                id={id}
+                defaultValue = {value}
+                onChange={onChange} 
+                className="form-select" 
+            > 
+                <option value="">Select a {name}</option> 
+                <option value="10">10</option> 
+                <option value="9">9</option> 
+                <option value="8">8</option> 
+                <option value="7">7</option> 
+                <option value="6">6</option> 
+                <option value="5">5</option> 
+                <option value="4">4</option> 
+                <option value="3">3</option> 
+                <option value="2">2</option> 
+                <option value="1">1</option> 
+            </Input> 
         </FormGroup>
     );
-};
+});
 
 export default ScoreDropdown;
